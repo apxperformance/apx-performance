cd3cd8d1e43d09b1842c7f9e57892c611879eabf
Peter Guerriera <pguerriera@gmail.com>
2026-01-14 03:42:07 +0000

    Manual code change

--- src/components/ui/popover.jsx
+++ src/components/ui/popover.jsx
@@ -1,13 +1,9 @@
 import * as React from "react"
 
 const Popover = ({ children }) => <div className="relative inline-block">{children}</div>
-const PopoverTrigger = ({ children, asChild, ...props }) => <div {...props}>{children}</div>
-const PopoverContent = React.forwardRef(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={`z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 ${className}`}
-    {...props}
-  />
+const PopoverTrigger = ({ children, ...props }) => <div {...props}>{children}</div>
+const PopoverContent = React.forwardRef(({ className, ...props }, ref) => (
+  <div ref={ref} className={`z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none ${className}`} {...props} />
 ))
 PopoverContent.displayName = "PopoverContent"
 
