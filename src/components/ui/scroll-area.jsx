c541e206e20510bcbdfe55eb8b32e9836ff64386
Peter Guerriera <pguerriera@gmail.com>
2026-01-14 03:39:41 +0000

    Manual code change

--- src/components/ui/scroll-area.jsx
+++ src/components/ui/scroll-area.jsx
@@ -1,16 +1,11 @@
 import * as React from "react"
 
 const ScrollArea = React.forwardRef(({ className, children, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={`relative overflow-auto ${className}`}
-    {...props}
-  >
+  <div ref={ref} className={`relative overflow-auto ${className}`} {...props}>
     {children}
   </div>
 ))
 ScrollArea.displayName = "ScrollArea"
-
-const ScrollBar = () => null // We don't need a custom scrollbar
+const ScrollBar = () => null
 
 export { ScrollArea, ScrollBar }
