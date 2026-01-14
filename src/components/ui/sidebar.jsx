30c27353d5c0364a2f1a39e94a47d3300ecff570
Peter Guerriera <pguerriera@gmail.com>
2026-01-14 03:21:53 +0000

    Manual code change

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -21,22 +21,6 @@ function useIsMobile() {
 
   return !!isMobile
 }
-
-function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(undefined)
-
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
-
-  return !!isMobile
-}
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
