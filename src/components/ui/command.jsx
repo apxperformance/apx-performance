3a8a7a8a19224f6564eb215609368c55da180aa8
apxperformance <sales@apxperformanceapp.com>
2026-01-15 01:28:09 -0500

    Update command.jsx

--- src/components/ui/command.jsx
+++ src/components/ui/command.jsx
@@ -38,7 +38,6 @@ const CommandInput = React.forwardRef(({ className, ...props }, ref) => (
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
-        // FIX: !text-foreground forces WHITE text using !important. 
         "flex h-10 w-full rounded-md bg-transparent py-3 text-sm !text-foreground outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
